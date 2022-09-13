e4a9726d97458b5d818e82a46517dcaa7ee743e6
Gautam kumar <gautamk1336@gmail.com>
2022-09-14 02:21:38 +0530

    version 1.1

--- script.js
+++ script.js
@@ -23,6 +23,25 @@ $(document).ready(function ()
     }
   });
 
+  // slide-up script
+  $(".scroll-up-btn").click(function ()
+  {
+    $("html").animate({ scrollTop: 0 });
+    // removing smooth scroll on slide-up button click
+    $("html").css("scrollBehavior", "auto");
+  });
+
+  $(".navbar .menu li a").click(function () {
+    // applying again smooth scroll on menu items click
+    $("html").css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $(".menu-btn").click(function () {
+    $(".navbar .menu").toggleClass("active");
+    $(".menu-btn i").toggleClass("active");
+  });
+
   // typing text animation script
   var typed = new Typed(".typing", {
     strings: ["Back-End Developer"],
@@ -36,5 +55,28 @@ $(document).ready(function ()
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
+  });
+
+  // owl carousel script
+  $(".carousel").owlCarousel({
+    margin: 20,
+    loop: true,
+    autoplay: true,
+    autoplayTimeOut: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      600: {
+        items: 2,
+        nav: false,
+      },
+      1000: {
+        items: 3,
+        nav: false,
+      },
+    },
   });
 });
