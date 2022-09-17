cddf20be8a0a9485c0c0e0982106c386efd59a5f
Gautam kumar <gautamk1336@gmail.com>
2022-09-17 23:31:07 +0530

    version 2.0

--- script.js
+++ script.js
@@ -48,14 +48,14 @@ $(document).ready(function ()
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Java Back-End Developer","Front End Developer", "Full Stack Developer"],
+    strings: ["Java Back-End Developer","Front End Web Developer", "Full Stack Web Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
   var typed = new Typed(".typing-2", {
-    strings: ["Java Back-End Developer", "Front End Developer", "Full Stack Developer"],
+    strings: ["Java Back-End Developer", "Front End Web Developer", "Full Stack Web Developer"],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
