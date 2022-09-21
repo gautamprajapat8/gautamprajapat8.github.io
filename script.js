c1f14428171bc9d73fad10e94941a75ab945c05a
Gautam kumar <gautamk1336@gmail.com>
2022-09-21 17:57:03 +0530

    some changes in Totle-commit

--- script.js
+++ script.js
@@ -156,11 +156,13 @@ const sleep = (milliseconds) => {
   return new Promise(resolve => setTimeout(resolve, milliseconds))
 }
 sleep(100).then(() => {
-  //do stuff
   let x=document.querySelector(".contrib-number").innerText;
   x=x.split(" ");
+  x=(+(x[0])--);
+  x=x-(x%10);
+
 
   document.querySelector("#contrib-number").innerText="";
-  document.querySelector("#contrib-number").innerText=x[0];
+  document.querySelector("#contrib-number").innerText=x+"+";
 })
 
