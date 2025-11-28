85fbba139a3b8ee46d907d9d30950de80c97e4eb
gautamprajapat8 <gautamk1336@gmail.com>
2025-11-28 17:31:44 +0530

    portfolio updated

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-import { sk, sky } from "./resources/skills.js";
-
-
-
 $(document).ready(function () {
   $(window).scroll(function () {
     // sticky navbar on scroll script
     if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
+      $(".navbar-navbar").addClass("sticky");
     }
     else {
-      $(".navbar").removeClass("sticky");
+      $(".navbar-navbar").removeClass("sticky");
     }
 
     // scroll-up button show/hide script
@@ -29,110 +25,48 @@ $(document).ready(function () {
     $("html").css("scrollBehavior", "auto");
   });
 
-  $(".navbar .menu li a").click(function () {
+  $(".navbar-navbar .menu li a").click(function () {
     // applying again smooth scroll on menu items click
     $("html").css("scrollBehavior", "smooth");
   });
 
   // toggle menu/navbar script
   $(".menu-btn").click(function () {
-    $(".navbar .menu").toggleClass("active");
+    $(".navbar-navbar .menu").toggleClass("active");
     $(".menu-btn i").toggleClass("active");
   });
 
   // typing text animation script
   var typed = new Typed(".typing", {
-    strings: ["Java Back-End Developer", "Front End Web Developer", "Full Stack Web Developer"],
+    strings: [
+              ".NET Backend Developer",
+              "ASP.NET Core Developer",
+              "C# Developer",
+              "Full Stack Web Developer (.NET + JS)",
+              "REST API Developer"
+            ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
-  var typed = new Typed(".typing-2", {
-    strings: ["Java Back-End Developer", "Front End Web Developer", "Full Stack Web Developer"],
+    var typed1 = new Typed(".typing-2", {
+    strings: [
+              ".NET Backend Developer",
+              "ASP.NET Core Developer",
+              "C# Developer",
+              "Full Stack Web Developer (.NET + JS)",
+              "REST API Developer"
+            ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true,
   });
 
-  // owl carousel script
-
-  if (screen.width <= 500) {
-    $(".carousel").owlCarousel({
-      margin: 10,
-      loop: true,
-      autoplay: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive:
-      {
-        0: {
-          items: 1,
-          nav: false,
-        },
-
-      },
-    });
-  }
-  else if (screen.width > 501 && screen.width <= 900) {
-    $(".carousel").owlCarousel({
-      margin: 15,
-      loop: true,
-      autoplay: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive:
-      {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        600: {
-          items: 2,
-          nav: false,
-        },
-
-      },
-    });
-  }
-  else {
-    $(".carousel").owlCarousel({
-      margin: 20,
-      loop: true,
-      autoplay: true,
-      autoplayTimeOut: 2000,
-      autoplayHoverPause: true,
-      responsive:
-      {
-        0: {
-          items: 1,
-          nav: false,
-        },
-        600: {
-          items: 2,
-          nav: false,
-        },
-        1000: {
-          items: 3,
-          nav: false,
-        },
-      },
-    });
-  }
 
 
 });
 
-if (screen.width >= 501 && screen.width <= 690) {
-  document.getElementById("skills-content").innerHTML = "";
-  document.getElementById("skills-content").innerHTML = (sk());
-  // console.log(sk());
-}
-if (screen.width >= 691 && screen.width <= 947) {
-  document.getElementById("skills-content").innerHTML = "";
-  document.getElementById("skills-content").innerHTML = (sky());
-  // console.log(sky());
-}
 
 
 
@@ -169,158 +103,6 @@ sleep(100).then(() => {
 
 
 
-// for project section
-var project = [
-  {
-    Imag: "Online_Trip_Management_System.jpg",
-    ProjTitle: "Online Trip Management System (Travel Junky)",
-    ProjDiscription: "This is Java_backend of online trip management System where the admin can manage all features, And the customer can also perform some task such as view routes, hotels, and etc.",
-    TeachStack: "Java | Hibernate | MySQL | Springboot ",
-    WorkDiscription: "It was collaborative project built by a team of 5 executed in 5 days and I was the Contributors in this.",
-    live: "https://drive.google.com/file/d/11yXgrT-ajtNEV3R9d5eTLKHqSkJYA-5e/view",
-    github: "https://github.com/prayagp8/kindhearted-afternoon-7542",
-    innerText: "LIVE ↗️"
-  },
-  {
-    Imag: "Course_monitoring_system.jpg",
-    ProjTitle: "Course monitoring system",
-    ProjDiscription: "This is a (mini) project for Course monitoring system, where Course Monitoring System will keep the records of all the courses, batches, faculties, and the day-wise update for every batch. It will also help the Faculty Head to maintain the course plan.",
-    TeachStack: "Java | MySQL | JDBC ",
-    WorkDiscription: "It was my individual project executed in 5 days.",
-    live: "https://drive.google.com/file/d/1YR7uTko9jhaeodC7NwlMtPSszhLDYK4c/view",
-    github: "https://github.com/gautamprajapat8/Course-monitoring-system",
-    innerText: "LIVE ↗️"
-  },
-  {
-    Imag: "lyst.jpg",
-    ProjTitle: "lyst.com (clone)",
-    ProjDiscription: "lyst is an e-commerce website for buying various types of Faison related products. We have tried our best to clone of the website.",
-    TeachStack: "HTML | CSS | JAVASCRIPT | ES6",
-    WorkDiscription: "It was collaborative project built by a team of 6 executed in 5 days and I was the co-leader in this.",
-    live: "https://glowing-panda-1c570f.netlify.app/",
-    github: "https://github.com/rhaegar7996/CW_Web19_lyst",
-    innerText: "LIVE ↗️"
-  },
-  {
-    Imag: "desktime.jpg",
-    ProjTitle: "desktime.com (clone)",
-    ProjDiscription: "deskTime is The ultimate all-in-one automatic time tracker website and We have tried our best to clone the website.",
-    TeachStack: "HTML | CSS | JAVASCRIPT | ES6",
-    WorkDiscription: "It was collaborative project built by a team of 5 executed in 5 days and I was the group's team leader in this.",
-    live: "https://gautamprajapat8.github.io/deskTime_Clone/",
-    github: "https://github.com/gautamprajapat8/deskTime_Clone",
-    innerText: "LIVE ↗️"
-  },
-  // {
-  //   Imag: "studentManagementSystem.jpg",
-  //   ProjTitle: "student management system",
-  //   ProjDiscription: "The student management system is a web Application where we can perform many operations on Student data. I have tried my best to make the website.",
-  //   TeachStack: "HTML | CSS | JAVASCRIPT | ES6",
-  //   WorkDiscription: "It was individual project which is given by masai school built by me in 1 day.",
-  //   live: "https://gautamprajapat8.github.io/studentManagementSystem/",
-  //   github: "https://github.com/gautamprajapat8/studentManagementSystem"
-  // },
-  // {
-  //   Imag: "youtubeClone.jpg",
-  //   ProjTitle: "YouTube (clone)",
-  //   ProjDiscription: "This is YouTube clone web Application where we can watch videos. I have tried my best to make the website.",
-  //   TeachStack: "HTML | CSS | JAVASCRIPT | ES6 | API",
-  //   WorkDiscription: "It was individual project which is given by masai school built by me in 1 day.",
-  //   live: "https://gautamprajapat8.github.io/YouTube_clone/",
-  //   github: "https://github.com/gautamprajapat8/YouTube_clone"
-  // },
-  // {
-  //   Imag: "jobApp.jpg",
-  //   ProjTitle: "jobApp",
-  //   ProjDiscription: "This is for the job apply web Application where we can perform many operations on job applied data. I have tried my best to make the website.",
-  //   TeachStack: "HTML | CSS | JAVASCRIPT | ES6 | API",
-  //   WorkDiscription: "It was individual project which is given by masai school built by me in 1 day.",
-  //   live: "https://gautamprajapat8.github.io/jobApp/",
-  //   github: "https://github.com/gautamprajapat8/jobApp",
-  // },
-  // {
-  //   Imag: "liveClock.jpg",
-  //   ProjTitle: "Live Clock",
-  //   ProjDiscription: "This is live Clock web Application where we can see the live Time. I have tried my best to make the website.",
-  //   TeachStack: "HTML | CSS | JAVASCRIPT",
-  //   WorkDiscription: "It was my first project .",
-  //   live: "https://gautamprajapat8.github.io/clock/",
-  //   github: "https://github.com/gautamprajapat8/clock"
-  // },
-
-];
-
-
-let x=false;
-
-project.forEach(function (data) {
-
-  let card = document.createElement("div");
-  card.setAttribute("class", "card");
-
-  let box = document.createElement("div");
-  box.setAttribute("class", "box");
-
-  let img = document.createElement("img");
-  img.src = "/resources/" + data.Imag;
-  img.alt = data.project_title;
-
-  let project_ditels = document.createElement("div");
-  project_ditels.setAttribute("class", "project-ditels");
-
-  let project_title = document.createElement("h3");
-  project_title.setAttribute("class", "project-title");
-  project_title.innerText = data.ProjTitle;
-
-  let p = document.createElement("p");
-  p.innerText = data.ProjDiscription;
-
-  let h4 = document.createElement("h4");
-  h4.innerText = "Tech-Stack";
-
-  let h5 = document.createElement("h5");
-  h5.innerText = data.TeachStack;
-
-
-  let li = document.createElement("li");
-  li.innerText = data.WorkDiscription;
-
-  let btn = document.createElement("div");
-  btn.setAttribute("class", "btn");
-
-
-  let live = document.createElement("a");
-  live.href = data.live;
-  live.target = "_blank";
-  live.innerText = data.innerText;
-
-  let github = document.createElement("a");
-  github.href = data.github;
-  github.target = "_blank";
-  github.innerText = " GITHUB ↗️";
-
-  btn.append(live, github);
-  project_ditels.append(project_title, p, h4, h5, li, btn);
-
-  box.append(img, project_ditels);
-
-  card.append(box);
-
-  // console.log(card);
-
-  document.querySelector("#carousel").append(card);
-});
-
-// For open resume in another window
-// document.querySelector(".resumeOpen").addEventListener("click", open);
-// function open() {
-//   window.location.href="https://gautamprajapat8.github.io/resources/Gautam_Kumar_Resume.pdf";
-// };
-
-// alert(window. innerWidth);
-
-
-
 document.querySelector(".resumeDow1").addEventListener("click", function()
 {
   window.open("resources/Gautam_Kumar_Resume.pdf", "_blank");
@@ -346,3 +128,5 @@ document.getElementById("send").addEventListener("click", function(){
   // window.location.replace("https://gautamprajapat8.github.io/");
   Location.reload();
 });
+
+
